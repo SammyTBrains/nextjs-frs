5c39f20d0a34ab5ad8b384f7f3780cca884876c5
SammyTBrains <josephsamuel034@gmail.com>
2023-03-20 21:38:14 +0100

    dum

--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -1,7 +1,19 @@
 //our-domain.com/news
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/important-news-page">Important news page</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
